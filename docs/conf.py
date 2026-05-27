e64039749686b03d33c1768fbb43a778c8d8d58a
Dionino Marco Giangrande <dioninomarco.giangrande@uniroma1.it>
2026-05-27 12:41:30 +0200

    Create _static folder to change HTML output and modify conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['css/custom.css',]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
